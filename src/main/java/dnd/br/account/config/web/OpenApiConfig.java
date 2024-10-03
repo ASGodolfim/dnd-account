f2771016eed8e4bca086446ae1ed49123df453bf
ASGodolfim <stonegodolfim@gmail.com>
2024-10-03 14:06:53 -0300

    feat: Open API Config

---
+++ src/main/java/dnd/br/account/config/web/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package dnd.br.account.config.web;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DnD Account and Character Creater")
+                        .version("v1")
+                        .description("Creator of Account and DnD Characters")
+                        .termsOfService("Accept")
+                        .license(
+                                new License()
+                                        .name("Apache 2.0")
+                                        .url("Link")
+                        )
+                );
+    }
+}
