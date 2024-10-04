53a56b7ab70ae21708d0544f27b14a2f65517f09
ASGodolfim <stonegodolfim@gmail.com>
2024-10-04 16:18:40 -0300

    feat: Multiclass/Multiclass sublcass Exception if not in list

--- src/main/java/dnd/br/account/config/services/CharacterServices.java
+++ src/main/java/dnd/br/account/config/services/CharacterServices.java
@@ -39,8 +39,10 @@ public class CharacterServices {
         var entity = Mapper.parseChar(character, Character.class);
 
         if (!entity.getMulticlass() || entity.getMulticlass() == null) {entity.setMulticlass(false);entity.setCharacterMulticlass(null);entity.setMulticlassSubclass(null);entity.setMulticlassLevel(0);}
-
-        entity.setCharacterLevel(entity.getClassLevel()+entity.getMulticlassLevel());
+        if (!checkClassSubclass(entity)) throw new IllegalArgumentException("Invalid Class or Subclass");
+        if (!checkMulticlassSubclass(entity)) throw new IllegalArgumentException("Invalid Multiclass or Multiclass Subclass");
+        entity.setCharacterLevel(entity.getClassLevel() + entity.getMulticlassLevel());
+        if (entity.getCharacterLevel()<1 || entity.getCharacterLevel()>20) throw new IllegalArgumentException("Level Under Minimum or Over Maximum");
 
         int conmodifier = -1;
         switch (entity.getConstitution()){
@@ -104,6 +106,8 @@ public class CharacterServices {
         if (!entity.getMulticlass() || entity.getMulticlass() == null) {entity.setMulticlass(false);entity.setCharacterMulticlass(null);entity.setMulticlassSubclass(null);entity.setMulticlassLevel(0);}
 
         if (!checkClassSubclass(entity)) throw new IllegalArgumentException("Invalid Class or Subclass");
+
+        if (!checkMulticlassSubclass(entity)) throw new IllegalArgumentException("Invalid Multiclass or Multiclass Subclass");
 
         entity.setCharacterLevel(entity.getClassLevel() + entity.getMulticlassLevel());
         if (entity.getCharacterLevel()<1 || entity.getCharacterLevel()>20) throw new IllegalArgumentException("Level Under Minimum or Over Maximum");
@@ -178,22 +182,10 @@ public class CharacterServices {
         return ResponseEntity.noContent().build();
     }
 
-    private boolean checkClassSubclass (Character character) {
 
-        List<String> classes = new ArrayList<>();
-        classes.add("Artificer");
-        classes.add("Barbarian");
-        classes.add("Bard");
-        classes.add("Cleric");
-        classes.add("Druid");
-        classes.add("Fighter");
-        classes.add("Monk");
-        classes.add("Paladin");
-        classes.add("Ranger");
-        classes.add("Rogue");
-        classes.add("Sorcerer");
-        classes.add("Warlock");
-        classes.add("Wizard");
+
+
+    private boolean checkClassSubclass (Character character) {
 
         List<String> artificer = new ArrayList<>();
         artificer.add("Alchemist");
@@ -202,116 +194,244 @@ public class CharacterServices {
         artificer.add("Battle Smith");
 
         List<String> barbarian = new ArrayList<>();
-        artificer.add("Berserker");
-        artificer.add("Storm Herald");
-        artificer.add("Totem Warriror");
-        artificer.add("Wild Magic");
+        barbarian.add("Berserker");
+        barbarian.add("Storm Herald");
+        barbarian.add("Totem Warriror");
+        barbarian.add("Wild Magic");
 
         List<String> bard = new ArrayList<>();
-        artificer.add("Lore");
-        artificer.add("Swords");
-        artificer.add("Valor");
-        artificer.add("Glamour");
+        bard.add("Lore");
+        bard.add("Swords");
+        bard.add("Valor");
+        bard.add("Glamour");
 
         List<String> cleric = new ArrayList<>();
-        artificer.add("Life");
-        artificer.add("Twilight");
-        artificer.add("Grave");
-        artificer.add("Tempest");
+        cleric.add("Life");
+        cleric.add("Twilight");
+        cleric.add("Grave");
+        cleric.add("Tempest");
 
         List<String> druid = new ArrayList<>();
-        artificer.add("Moon");
-        artificer.add("Dreams");
-        artificer.add("Spores");
-        artificer.add("Stars");
+        druid.add("Moon");
+        druid.add("Dreams");
+        druid.add("Spores");
+        druid.add("Stars");
 
         List<String> fighter = new ArrayList<>();
-        artificer.add("Battle Master");
-        artificer.add("Echo Knight");
-        artificer.add("Rune Knight");
-        artificer.add("Samurai");
+        fighter.add("Battle Master");
+        fighter.add("Echo Knight");
+        fighter.add("Rune Knight");
+        fighter.add("Samurai");
 
         List<String> monk = new ArrayList<>();
-        artificer.add("Astral Self");
-        artificer.add("Drunken Master");
-        artificer.add("Four Elements");
-        artificer.add("Open Hand");
+        monk.add("Astral Self");
+        monk.add("Drunken Master");
+        monk.add("Four Elements");
+        monk.add("Open Hand");
 
         List<String> paladin = new ArrayList<>();
-        artificer.add("Ancients");
-        artificer.add("Conquest");
-        artificer.add("Glory");
-        artificer.add("Oathbreaker");
+        paladin.add("Ancients");
+        paladin.add("Conquest");
+        paladin.add("Glory");
+        paladin.add("Oathbreaker");
 
         List<String> ranger = new ArrayList<>();
-        artificer.add("Beast Master");
-        artificer.add("Gloom Stalker");
-        artificer.add("Hunter");
-        artificer.add("Swarmkeeper");
+        ranger.add("Beast Master");
+        ranger.add("Gloom Stalker");
+        ranger.add("Hunter");
+        ranger.add("Swarmkeeper");
 
         List<String> rogue = new ArrayList<>();
-        artificer.add("Arcane Trickster");
-        artificer.add("Assassin");
-        artificer.add("Mastermind");
-        artificer.add("Phantom");
+        rogue.add("Arcane Trickster");
+        rogue.add("Assassin");
+        rogue.add("Mastermind");
+        rogue.add("Phantom");
 
         List<String> sorcerer = new ArrayList<>();
-        artificer.add("Draconic Bloodline");
-        artificer.add("Divine Soul");
-        artificer.add("Shadow Magic");
-        artificer.add("Storm Sorcery");
+        sorcerer.add("Draconic Bloodline");
+        sorcerer.add("Divine Soul");
+        sorcerer.add("Shadow Magic");
+        sorcerer.add("Storm Sorcery");
 
         List<String> warlock = new ArrayList<>();
-        artificer.add("Archfey");
-        artificer.add("Fiend");
-        artificer.add("Hexblade");
-        artificer.add("Undead");
+        warlock.add("Archfey");
+        warlock.add("Fiend");
+        warlock.add("Hexblade");
+        warlock.add("Undead");
 
         List<String> wizard = new ArrayList<>();
-        artificer.add("Bladesinging");
-        artificer.add("Graviturgy");
-        artificer.add("Scribes");
-        artificer.add("War Magic");
+        wizard.add("Bladesinging");
+        wizard.add("Graviturgy");
+        wizard.add("Scribes");
+        wizard.add("War Magic");
+
+        var subclass = character.getSubclass();
 
         switch (character.getCharacterClass()) {
             case "Artificer":
-                if (artificer.contains(character.getSubclass())) return true;
+                if (artificer.contains(subclass)) return true;
                 break;
             case "Barbarian":
-                if (barbarian.contains(character.getSubclass())) return true;
+                if (barbarian.contains(subclass)) return true;
                 break;
             case "Bard":
-                if (bard.contains(character.getSubclass())) return true;
+                if (bard.contains(subclass)) return true;
                 break;
             case "Cleric":
-                if (cleric.contains(character.getSubclass())) return true;
+                if (cleric.contains(subclass)) return true;
                 break;
             case "Druid":
-                if (druid.contains(character.getSubclass())) return true;
+                if (druid.contains(subclass)) return true;
                 break;
             case "Fighter":
-                if (fighter.contains(character.getSubclass())) return true;
+                if (fighter.contains(subclass)) return true;
                 break;
             case "Monk":
-                if (monk.contains(character.getSubclass())) return true;
+                if (monk.contains(subclass)) return true;
                 break;
             case "Paladin":
-                if (paladin.contains(character.getSubclass())) return true;
+                if (paladin.contains(subclass)) return true;
                 break;
             case "Ranger":
-                if (ranger.contains(character.getSubclass())) return true;
+                if (ranger.contains(subclass)) return true;
                 break;
             case "Rogue":
-                if (rogue.contains(character.getSubclass())) return true;
+                if (rogue.contains(subclass)) return true;
                 break;
             case "Sorcerer":
-                if (sorcerer.contains(character.getSubclass())) return true;
+                if (sorcerer.contains(subclass)) return true;
                 break;
             case "Warlock":
-                if (warlock.contains(character.getSubclass())) return true;
+                if (warlock.contains(subclass)) return true;
                 break;
             case "Wizard":
-                if (wizard.contains(character.getSubclass())) return true;
+                if (wizard.contains(subclass)) return true;
+                break;
+            default:
+        }
+        return false;
+    }
+
+    private boolean checkMulticlassSubclass (Character character) {
+        List<String> artificer = new ArrayList<>();
+        artificer.add("Alchemist");
+        artificer.add("Armorer");
+        artificer.add("Artillerist");
+        artificer.add("Battle Smith");
+
+        List<String> barbarian = new ArrayList<>();
+        barbarian.add("Berserker");
+        barbarian.add("Storm Herald");
+        barbarian.add("Totem Warriror");
+        barbarian.add("Wild Magic");
+
+        List<String> bard = new ArrayList<>();
+        bard.add("Lore");
+        bard.add("Swords");
+        bard.add("Valor");
+        bard.add("Glamour");
+
+        List<String> cleric = new ArrayList<>();
+        cleric.add("Life");
+        cleric.add("Twilight");
+        cleric.add("Grave");
+        cleric.add("Tempest");
+
+        List<String> druid = new ArrayList<>();
+        druid.add("Moon");
+        druid.add("Dreams");
+        druid.add("Spores");
+        druid.add("Stars");
+
+        List<String> fighter = new ArrayList<>();
+        fighter.add("Battle Master");
+        fighter.add("Echo Knight");
+        fighter.add("Rune Knight");
+        fighter.add("Samurai");
+
+        List<String> monk = new ArrayList<>();
+        monk.add("Astral Self");
+        monk.add("Drunken Master");
+        monk.add("Four Elements");
+        monk.add("Open Hand");
+
+        List<String> paladin = new ArrayList<>();
+        paladin.add("Ancients");
+        paladin.add("Conquest");
+        paladin.add("Glory");
+        paladin.add("Oathbreaker");
+
+        List<String> ranger = new ArrayList<>();
+        ranger.add("Beast Master");
+        ranger.add("Gloom Stalker");
+        ranger.add("Hunter");
+        ranger.add("Swarmkeeper");
+
+        List<String> rogue = new ArrayList<>();
+        rogue.add("Arcane Trickster");
+        rogue.add("Assassin");
+        rogue.add("Mastermind");
+        rogue.add("Phantom");
+
+        List<String> sorcerer = new ArrayList<>();
+        sorcerer.add("Draconic Bloodline");
+        sorcerer.add("Divine Soul");
+        sorcerer.add("Shadow Magic");
+        sorcerer.add("Storm Sorcery");
+
+        List<String> warlock = new ArrayList<>();
+        warlock.add("Archfey");
+        warlock.add("Fiend");
+        warlock.add("Hexblade");
+        warlock.add("Undead");
+
+        List<String> wizard = new ArrayList<>();
+        wizard.add("Bladesinging");
+        wizard.add("Graviturgy");
+        wizard.add("Scribes");
+        wizard.add("War Magic");
+
+        var subclass = character.getMulticlassSubclass();
+
+        switch (character.getCharacterMulticlass()) {
+            case "Artificer":
+                if (artificer.contains(subclass)) return true;
+                break;
+            case "Barbarian":
+                if (barbarian.contains(subclass)) return true;
+                break;
+            case "Bard":
+                if (bard.contains(subclass)) return true;
+                break;
+            case "Cleric":
+                if (cleric.contains(subclass)) return true;
+                break;
+            case "Druid":
+                if (druid.contains(subclass)) return true;
+                break;
+            case "Fighter":
+                if (fighter.contains(subclass)) return true;
+                break;
+            case "Monk":
+                if (monk.contains(subclass)) return true;
+                break;
+            case "Paladin":
+                if (paladin.contains(subclass)) return true;
+                break;
+            case "Ranger":
+                if (ranger.contains(subclass)) return true;
+                break;
+            case "Rogue":
+                if (rogue.contains(subclass)) return true;
+                break;
+            case "Sorcerer":
+                if (sorcerer.contains(subclass)) return true;
+                break;
+            case "Warlock":
+                if (warlock.contains(subclass)) return true;
+                break;
+            case "Wizard":
+                if (wizard.contains(subclass)) return true;
                 break;
             default:
         }
